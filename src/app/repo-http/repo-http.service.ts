8229d7435b96fb8d22582f65ef5accadd0964ccd
aline-bebe <ingabirealine40@gmail.com>
2019-09-04 14:50:18 +0200

    styling

--- src/app/repo-http/repo-http.service.ts
+++ src/app/repo-http/repo-http.service.ts
@@ -1,47 +1,47 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {environment } from '../../environments/environment';
-import {User} from '../user';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import {environment } from '../../environments/environment';
+// import {User} from '../user';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class RepoHttpService {
-  user:User;
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class RepoHttpService {
+//   user:User;
 
-  constructor(private http:HttpClient) {
-    this.user = new User("","","","");
-   }
+//   constructor(private http:HttpClient) {
+//     this.user = new User("","","","");
+//    }
 
 
-   repohttp(){
-    interface ApiResponse{
-      login: string;
-      name: any;
-      email: string;
-    //  bio: string;
-      avatar_url: string;
-    }
-    let promise = new Promise((resolve,reject)=>{
-      this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
-        this.user.login = response.login
-        this.user.name = response.name
-        this.user.email = response.email
-        // this.user.bio= response.bio
-        this.user.avatar_url = response.avatar_url
+//    repohttp(){
+//     interface ApiResponse{
+//       login: string;
+//       name: any;
+//       email: string;
+//     //  bio: string;
+//       avatar_url: string;
+//     }
+//     let promise = new Promise((resolve,reject)=>{
+//       this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+//         this.user.login = response.login
+//         this.user.name = response.name
+//         this.user.email = response.email
+//         // this.user.bio= response.bio
+//         this.user.avatar_url = response.avatar_url
 
-        resolve()
-      },
-      // error=>{
-      //   this.quote.quote = "Never, never, never give up"
-      //   this.quote.author = "Winston Churchill"
+//         resolve()
+//       },
+//       // error=>{
+//       //   this.quote.quote = "Never, never, never give up"
+//       //   this.quote.author = "Winston Churchill"
 
-      //   reject(error)
-      // }
-      )
+//       //   reject(error)
+//       // }
+//       )
 
-    })
-    return promise
-  }
+//     })
+//     return promise
+//   }
 
-}
+// }
